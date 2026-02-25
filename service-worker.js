9e7af0ae1aa1078757c714299dc8ffb197ae1b7c
playkumar03 <playkumar03@gmail.com>
2026-02-26 01:09:47 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,20 @@
+const CACHE_NAME = "dilse-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json"
+];
+
 self.addEventListener("install", (event) => {
-  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener("activate", (event) => {
-  console.log("Service Worker Activated");
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
 });
